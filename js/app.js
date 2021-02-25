add531a2fc639daab51751591d1e462316edbb78
Silvina Bordon <silbordon.89@gmail.com>
2021-02-25 13:38:02 -0300

    Create/Se agregan los modales utilizando la libreria SweetAlert y se agregan funciones temporales a los botones info y reset

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 
 //General
-const grid = document.getElementById('grid');
-const emojis = ['🎃','⚰','👻','🕸','🧛🏻‍♀️', '🦇','\uD83E\uDDDF\u200D\u2642\uFE0F']; 
-let level = 7;
+const grid = document.getElementById('grid'); //grid container
+const emojis = ['🎃','⚰','👻','🕸','🧛🏻‍♀️','🦇','\uD83E\uDDDF\u200D\u2642\uFE0F']; 
+const infoButton = document.getElementById('info'); //Button info
+const resetButton = document.getElementById('reset'); //Button reset
 
 
 //Create Grid
-const nuevoArr = [''];
+let nuevoArr = [''];
+let level = 0;
 
 const createGrid = ()=>{
+  nuevoArr = [''];
   grid.innerHTML = '';
-  for(i=0; i < level; i ++ ){
+  for(i=0; i < level; i++ ){
     for(j=0; j < level; j++){
       nuevoArr.push(emojis[(Math.random() * (emojis.length -1)).toFixed(0)]);
-    };
+    }; 
   };
   
-  for(i=1; i < nuevoArr.length; i ++ ){
+  for(i=1; i < nuevoArr.length; i++){
     const caja = document.createElement('div');
     caja.style.width = `${500/level}px`;
     caja.style.height = `${500/level}px`;
@@ -26,4 +29,16 @@ const createGrid = ()=>{
   };
   
 };  
-createGrid();
+
+
+  
+//   Show welcome modal
+  window.onload = swallModalWelcom();
+
+
+  // Buttons Events 
+  infoButton.addEventListener('click', swallModalWelcom);
+  resetButton.addEventListener('click', swalResetGame);
+
+ 
+
---
+++ js/modales.js
@@ -0,0 +1,122 @@
+
+//Function level-Game
+const levelGame = () =>{
+    swal({
+      title: "Nuevo Juego",
+      text: `Selecciona una dificultad`,
+      buttons: {
+        facil: {
+          text: "Facil",
+          value: "facil"
+        },
+        medio: {
+          text: "Medio",
+          value: "medio"
+        },
+        dificil: {
+          text: "dificil",
+          value: "dificil"
+        },
+      },
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+    })
+    .then((value) => {
+      switch (value) {
+        case "facil":
+          level = 9;
+          break;
+        case "medio":
+          level = 8;
+          break;
+    
+        case "dificil":
+          level = 7;
+          break;
+        default:
+      }
+      createGrid();
+    });
+    };
+  
+  
+
+  //Modal Welcome
+  const swallModalWelcom = () => {
+    swal({
+      title: "¡Bienvenida!",
+      text: `En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo,
+          ya sea en fila o columna. Para eso, selecciona un ítem y a
+          continuación un ítem adyacente para intercambiarlos de lugar.
+  
+          Si se forma un grupo, esos ítems se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!
+  
+          CONTROLES
+          Click izquierdo: selección
+          Enter o Espacio: selección
+          Flechas o WASD: movimiento e intercambio`,
+      button: "A Jugar",
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+  
+    })
+    .then(levelGame)
+  };
+  
+  
+  //Modal Reset Game and choose new level
+  const swalResetGame = ()=>{
+    swal({
+      title: "Reiniciar juego?",
+      text: `Perderás todo tu puntaje acumulado`,
+      buttons: {
+        cancel: "Cancelar",
+        confirm: "Nuevo Juego",
+      },
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+    })
+    .then((nuevoJuego) => nuevoJuego ? levelGame() : null);
+  };
+  
+  
+  // Modal Game Over y points
+  let puntajeFinal;
+  const swalGameOver = () => {
+    swal({
+      title: "¡Juego Terminado!",
+      text: `Puntaje Final: ${puntajeFinal}`,
+      buttons: {
+        newGame: {
+          text: "Nuevo Juego",
+          value: "nuevoJuego"
+        },
+        reset: {
+          text: "Reiniciar",
+          value: "reiniciar"
+        },
+      },
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+    })
+    .then((value) => {
+      switch (value) {
+        case "nuevoJuego":
+          levelGame();
+          break;
+        case "reiniciar":
+  //funcion reiniciar
+  
+          break;
+        default:
+      };
+    });
+  
+  };
+    
+//   swalGameOver();
+//   swallModalWelcom();
+  // swalResetGame();
+  
+  
+
