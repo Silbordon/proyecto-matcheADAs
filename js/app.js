836fc17ddbe1a00e27a8b6a030e2e8ffb79dae5c
Silvina Bordon <silbordon.89@gmail.com>
2021-03-10 17:39:46 -0300

    UPDATE/Se agrega section seleccion items y se reenderizan funciones existentes

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const grid = document.getElementById('grid'); //grid container
 const emojis = ['🎃', '⚰', '👻', '🕸', '🧛🏻‍♀️', '🦇', '\uD83E\uDDDF\u200D\u2642\uFE0F'];
 const infoButton = document.getElementById('info'); //Button info
 const resetButton = document.getElementById('reset'); //Button reset
+let gridArr = [];
+
+
 
 //Timer
 const timer = document.getElementById("timer");
@@ -15,31 +18,74 @@ let restTime;
 let optionWelcome = true;
 
 
+
+// Generate item random
+const getRandom = () =>
+  emojis[(Math.random() * (emojis.length - 1)).toFixed(0)];
+
+
+
+// Generate DIV-EMOJI-BOX
+const getBox = (x,y) => {
+  const box = document.createElement("div");
+  box.dataset.x = x;
+  box.dataset.y = y;
+  box.innerHTML = gridArr[i][j];
+  box.style.width = `${500 / level}px`;
+  box.style.height = `${500 / level}px`;
+  grid.appendChild(box);
+  box.addEventListener("click", selectBox);
+};
+
+
+// Timer Function Minutes
+const secondsToMinutes = (time) => {
+  let seconds = time % 60;
+  let minutes = ((time - seconds) / 60) % 60;
+  let txtSeconds = seconds < 10 ? "0" + seconds : seconds;
+  let txtMinutes = minutes < 10 ? "0" + minutes : minutes;
+  return (timer.innerHTML = `${txtMinutes}:${txtSeconds}`);
+};
+
+
+
+//Timer Function
+const myTimer = () => {
+  restTime = setInterval(() => {
+    if (time <= 30 && time > 0) {
+      time = time - 1
+      secondsToMinutes(time);
+    } else {
+      swalGameOver();
+    }
+  }
+    , 1000);
+};
+
+function stopTimer() {
+  clearInterval(restTime);
+}
+
+
+
 //Create Grid
 let level = 0;
 
 const createGrid = () => {
   time = 30;
+  gridArr = [];
   timer.innerHTML = `00:${time}`
   grid.innerHTML = '';
   for (i = 0; i < level; i++) {
+    gridArr[i] = [];
     for (j = 0; j < level; j++) {
-      const caja = document.createElement('div');
-      caja.innerHTML = emojis[(Math.random() * (emojis.length - 1)).toFixed(0)];
-      caja.style.width = `${500 / level}px`;
-      caja.style.height = `${500 / level}px`;
-      caja.dataset.x = i;
-      caja.dataset.y = j;
-      console.log(i,j)
-      grid.appendChild(caja);
-      caja.addEventListener('click', () =>{
-        caja.classList.add('select');
-      })
+      gridArr[i][j] = getRandom();
+      getBox(i,j);
     };
     twemoji.parse(document.body);
   };
 
-  restTime = setInterval(myTimer, 1000);
+  myTimer();
   return level
 };
 
@@ -54,25 +100,41 @@ infoButton.addEventListener('click', swallModalWelcom);
 resetButton.addEventListener('click', swalResetGame);
 
 
-//Timer
-const myTimer = () => {
-  if (time <= 30 && time > 0) {
-    time = time - 1
-    let seconds = time % 60;
-    let minutes = ((time - seconds) / 60) % 60;
-    let txtSeconds = seconds < 10 ? '0' + seconds : seconds
-    let txtMinutes = minutes < 10 ? '0' + minutes : minutes
-    timer.innerHTML = `${txtMinutes}:${txtSeconds}`
+//Items Select
+
+const selectBox = e => {
+  let clickedItem = document.querySelector('.select')
+  // console.log(e.path[1])
+  if (clickedItem) {
+    console.log(isAdjancent(clickedItem, e.path[1]))
 
   } else {
-    swalGameOver();
+    e.path[1].classList.add('select')
   }
+}
+
+const isAdjancent = (box1, box2) => {
+  const datax1 = Number(box1.dataset.x);
+  const datax2 = Number(box2.dataset.x);
+  const datay1 = Number(box1.dataset.y);
+  const datay2 = Number(box2.dataset.y);
+  console.log(box1.dataset.x)
+  console.log(box1.dataset.y)
+
+
+  if (
+    (datax1 === datax2 && datay1 === datay2 + 1) || //abajo
+      (datax1 === datax2 && datay1 === datay2 - 1) || //arriba
+      (datax1 === datax2 + 1 && datay1 === datay2) || //derecha
+      (datax1 === datax2 - 1 && datay1 === datay2) //izquierda
+  ) {
+    return true
+  }
+  return false
 };
 
 
-function stopTimer() {
-  clearInterval(restTime);
-}
 
 
-//Items Select
+
+
